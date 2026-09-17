32909043650bb74bef1b14f5af0d85be74ecea1f
Albert <albertsansgomez@gmail.com>
2026-09-18 00:43:53 +0200

    Agregar estilos parte superior página categorías

--- app/(app)/[slug]/page.tsx
+++ app/(app)/[slug]/page.tsx
@@ -10,8 +10,22 @@ export default async function CategoryPage({ params }: PageProps) {
   const { slug } = await params;
 
   return (
-    <main>
-      <h1>Categoria: {slug}</h1>
+    <>
+      <div className="bg-amber-500 pt-12 pr-20 pb-8 pl-20">
+        <h1 className="font-normal text-[46px] leading-[46px]">
+          Categoria: Mesas
+        </h1>
+        <p className="font-normal text-[15px] leading-5 w-[644px] mt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
       <ul>
         <li>
           <Link href="/product/litto-table-made-from-oak-veneer/">Litto</Link>
@@ -27,6 +41,6 @@ export default async function CategoryPage({ params }: PageProps) {
           </Link>
         </li>
       </ul>
-    </main>
+    </>
   );
 }
