abde1fb2d5b0f029fcdbdbfc30143bbf0e0c3ea7
Albert <albertsansgomez@gmail.com>
2026-09-17 23:38:23 +0200

    Crear layout general

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from '@/components/layout/Header';
+
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen w-full">
+      <Header />
+      <main className="w-full">
+        {children}
+      </main>
+    </div>
+  );
+}
