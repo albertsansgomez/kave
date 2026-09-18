921c5fa6bc164b61c5cf3ee6e1dfab7ced1a2d86
Albert <albertsansgomez@gmail.com>
2026-09-18 09:13:17 +0200

    Crear util cn

---
+++ utils/cn.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Combina clases CSS de forma condicional y 
+ * resuelve conflictos entre clases de Tailwind CSS.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
