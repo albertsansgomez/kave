16a31d110e512c1a8a431e81d6128fdcf34f3617
Albert <albertsansgomez@gmail.com>
2026-09-18 09:32:23 +0200

    Formatear código

--- utils/cn.ts
+++ utils/cn.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /**
- * Combina clases CSS de forma condicional y 
+ * Combina clases CSS de forma condicional y
  * resuelve conflictos entre clases de Tailwind CSS.
  */
 export function cn(...inputs: ClassValue[]) {
