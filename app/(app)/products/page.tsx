f9a0614c308618cdef0c202c1f7d50760a2fbc2f
Albert <albertsansgomez@gmail.com>
2026-09-20 17:55:05 +0200

    Limpiar código

--- app/(app)/products/page.tsx
+++ app/(app)/products/page.tsx
@@ -1,4 +1,3 @@
-import Product from '@/components/ui/Product';
 import Pagination from '@/components/ui/Pagination';
 import PageIntro from '@/components/ui/PageIntro';
 
@@ -9,12 +8,6 @@ export default function Products() {
         title="Todos los productos"
         description="Explora todas nuestras colecciones y encuentra esas piezas que hacen de tu casa un lugar más tuyo."
       />
-
-      <section className="grid grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-[37px] lg:gap-x-2 lg:gap-y-[60px]">
-        {Array.from({ length: 4 }, (_, index) => (
-          <Product key={index} />
-        ))}
-      </section>
 
       <Pagination
         className="mt-[60px] mb-[56px]"
