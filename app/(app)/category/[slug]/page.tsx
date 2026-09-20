79fc89b94cea00016c791424847dc3e2b06d16ed
Albert <albertsansgomez@gmail.com>
2026-09-20 17:57:54 +0200

    Agregar suspense en página categoria

--- app/(app)/category/[slug]/page.tsx
+++ app/(app)/category/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import PageIntro from '@/components/ui/PageIntro';
 import ProductList from '@/components/ui/ProductList';
 
@@ -16,6 +18,7 @@ export default async function CategoryPage({
 }: PageProps) {
   const { slug } = await params;
   const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
 
   return (
     <>
@@ -23,8 +26,9 @@ export default async function CategoryPage({
         title="Mesas"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       />
-
-      <ProductList currentPage={page ? parseInt(page) : 1} />
+      <Suspense fallback={<>Loading CategoryHome</>}>
+        <ProductList currentPage={currentPage} />
+      </Suspense>
     </>
   );
 }
