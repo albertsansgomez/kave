e1fa9b536d7c45fd7b10024c17fb8434d372fee1
Albert <albertsansgomez@gmail.com>
2026-09-20 16:45:21 +0200

    Ajustes en el servicio getCategories y mock categories

--- services/categories.ts
+++ services/categories.ts
@@ -8,13 +8,13 @@ import { CATEGORIES_ENDPOINT } from '@/services/api/categories';
 const USE_MOCK = true;
 
 export async function getCategories(
-  url: string = CATEGORIES_ENDPOINT,
+  { page }: { page: number },
 ): Promise<Response<Categories>> {
   if (USE_MOCK) {
-    return getCategoriesMock(url);
+    return getCategoriesMock(page);
   }
 
-  const response = await fetch(url);
+  const response = await fetch(`${CATEGORIES_ENDPOINT}?page=${page}`);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch categories: ${response.status}`);
--- services/mock/categories.ts
+++ services/mock/categories.ts
@@ -1,6 +1,8 @@
 import type { Categories } from '@/types/categories';
 import type { Response } from '@/types/response';
 
+import { CATEGORIES_ENDPOINT } from '../api/categories';
+
 const PAGE_SIZE = 20;
 
 const categories: Categories[] = [
@@ -787,24 +789,10 @@ const categories: Categories[] = [
   },
 ];
 
-function getPage(url: string): number {
-  return Number(new URL(url).searchParams.get('page') ?? 1);
-}
-
-function getPageUrl(url: string, page: number): string {
-  const parsedUrl = new URL(url);
-
-  parsedUrl.searchParams.set('page', String(page));
-
-  return parsedUrl.toString();
-}
-
 export async function getCategoriesMock(
-  url: string,
+  page: number
 ): Promise<Response<Categories>> {
   await new Promise((resolve) => setTimeout(resolve, 300));
-
-  const page = getPage(url);
 
   const start = (page - 1) * PAGE_SIZE;
   const end = start + PAGE_SIZE;
@@ -814,8 +802,8 @@ export async function getCategoriesMock(
 
   return {
     count: categories.length,
-    next: page < totalPages ? getPageUrl(url, page + 1) : null,
-    previous: page > 1 ? getPageUrl(url, page - 1) : null,
+    next: page < totalPages ? (`${CATEGORIES_ENDPOINT}?page=${page + 1}`) : null,
+    previous: page > 1 ? (`${CATEGORIES_ENDPOINT}?page=${page - 1}`) : null,
     results,
   };
 }
