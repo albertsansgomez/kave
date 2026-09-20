16bca643402148242fdff71361e75347ccb54f15
Albert <albertsansgomez@gmail.com>
2026-09-20 18:40:37 +0200

    LImpiar código

--- app/(app)/wishlist/page.tsx
+++ app/(app)/wishlist/page.tsx
@@ -1,5 +1,3 @@
-import Product from '@/components/ui/Product';
-import Pagination from '@/components/ui/Pagination';
 import PageIntro from '@/components/ui/PageIntro';
 
 export default function Wishlist() {
@@ -8,18 +6,6 @@ export default function Wishlist() {
       <PageIntro
         title="Wishlist"
         description="Aquí vienen las piezas que te gustan."
-      />
-
-      <section className="grid grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-[37px] lg:gap-x-2 lg:gap-y-[60px]">
-        {Array.from({ length: 4 }, (_, index) => (
-          <Product key={index} />
-        ))}
-      </section>
-
-      <Pagination
-        className="mt-[60px] mb-[56px]"
-        currentPage={1}
-        totalPages={7}
       />
     </>
   );
