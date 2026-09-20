57b8e9b8074c57f88489e0b1cfac276528083c0a
Albert <albertsansgomez@gmail.com>
2026-09-20 16:47:05 +0200

    Ajustes etiquetas en componente PageIntro

--- components/ui/PageIntro.tsx
+++ components/ui/PageIntro.tsx
@@ -9,9 +9,9 @@ export default function PageIntro({ title, description }: PageIntroProps) {
   return (
     <div className="pt-3 pr-6 pb-6 pl-6 lg:pt-12 lg:pr-20 lg:pb-8 lg:pl-20">
       <h1 className="font-normal text-[46px] leading-[46px]">{title}</h1>
-      <p className="font-normal text-[15px] leading-5 lg:w-[644px] mt-3">
+      <div className="font-normal text-[15px] leading-5 lg:w-[644px] mt-3">
         {description}
-      </p>
+      </div>
     </div>
   );
 }
