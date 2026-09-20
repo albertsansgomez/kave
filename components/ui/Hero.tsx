53edfde89392bc10187f80ce9d418a75bd1029c7
Albert <albertsansgomez@gmail.com>
2026-09-20 09:52:52 +0200

    Agregar video en componente Hero

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -7,14 +7,16 @@ export default function Hero() {
       aria-labelledby="hero-title"
       className="relative min-h-[calc(100vh-62px)] w-full"
     >
-      <Image
-        src="https://picsum.photos/375/300"
-        fill
-        priority
-        alt="Lorem ipsum dolor sit amet"
-        className="object-cover"
-        sizes="100vw"
-      />
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 h-full w-full object-cover"
+        aria-hidden="true"
+      >
+        <source src="/videos/SS26_Spot_Slide_Desktop.mp4" type="video/mp4" />
+      </video>
 
       <div className="absolute w-full h-full">
         <div className="absolute w-full bottom-0 left-0 pt-[33px] pr-4 pb-4 pl-6 lg:pt-8 lg:pr-[72px] lg:pb-10 lg:pl-[72px] flex flex-col gap-6 lg:gap-[120px] lg:flex-row items-start lg:justify-between lg:items-end">
@@ -31,7 +33,7 @@ export default function Hero() {
               Una manera muy nuestra de estar.
             </h1>
           </div>
-          <div className="flex items-end gap-2 bg-green-600">
+          <div className="flex items-end gap-2">
             <Link
               href="#"
               className="shrink-0 whitespace-nowrap text-center font-normal bg-white hover:bg-gray-400 hover:text-white pt-2 pr-5 pb-2 pl-5 h-10"
