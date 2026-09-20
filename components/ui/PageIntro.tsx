6474d9d09ccb882e9462dc49740000dd58f40497
Albert <albertsansgomez@gmail.com>
2026-09-20 16:30:54 +0200

    Ajustes componente PageIntro

--- components/ui/PageIntro.tsx
+++ components/ui/PageIntro.tsx
@@ -1,6 +1,8 @@
+import { ReactNode } from 'react';
+
 interface PageIntroProps {
   title: string;
-  description: string;
+  description: string | ReactNode;
 }
 
 export default function PageIntro({ title, description }: PageIntroProps) {
