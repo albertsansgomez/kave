be5c611d3708f4fde947392e40dab85e62eb5bc9
Albert <albertsansgomez@gmail.com>
2026-09-21 12:22:59 +0200

    Formatear código

--- app/(app)/wishlist/page.tsx
+++ app/(app)/wishlist/page.tsx
@@ -3,7 +3,7 @@
 import { useFavorites } from '@/contexts/FavoritesContext';
 
 import PageIntro from '@/components/ui/PageIntro';
-import ProductComponent from '@/components/ui/Product';
+import Product from '@/components/ui/Product';
 
 export default function Wishlist() {
   const { favorites } = useFavorites();
@@ -16,12 +16,9 @@ export default function Wishlist() {
       />
       <section className="grid grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-[37px] lg:gap-x-2 lg:gap-y-[60px]">
         {favorites.map((product, index) => (
-          <ProductComponent
-            key={product.sku}
-            priority={index <= 4}
-            product={product}
-          />
-        ))}</section>
+          <Product key={product.sku} priority={index <= 4} product={product} />
+        ))}
+      </section>
     </>
   );
 }
