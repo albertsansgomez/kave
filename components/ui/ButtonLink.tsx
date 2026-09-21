90036f5dfe4bfe93a2a00b2d129992eceaed1a26
Albert <albertsansgomez@gmail.com>
2026-09-21 23:45:09 +0200

    Fijar centrado vertical en componente ButtonLink

--- components/ui/ButtonLink.tsx
+++ components/ui/ButtonLink.tsx
@@ -11,7 +11,7 @@ interface ButtonLinkProps {
 export default function ButtonLink({ text, href, className }: ButtonLinkProps) {
   const buttonClassName = cn(
     className,
-    'h-10 whitespace-nowrap bg-white px-5 py-2 text-center text-[14px] hover:bg-gray-400 hover:text-white',
+    'h-10 whitespace-nowrap bg-white px-5 py-2 flex items-center justify-center text-[14px] hover:bg-gray-400 hover:text-white',
   );
 
   return (
